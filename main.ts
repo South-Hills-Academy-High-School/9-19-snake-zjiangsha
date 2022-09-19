9aab5add9b6ed7fb2e0913c3549333db31ad10b3
zjiangsha <113388192+zjiangsha@users.noreply.github.com>
2022-09-19 13:54:02 -0700

    almost done!

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     vy = 1
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    if (atefoodflag == 0) {
+    if (atefoodflag == 0 && snakehead.overlapsWith(snakefood)) {
         snakebody.unshift(sprites.create(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -48,11 +48,13 @@ let vy = 0
 let vx = 0
 let foody = 0
 let foodx = 0
+let snakefood: Sprite = null
+let snakehead: Sprite = null
 let atefoodflag = 0
 let snakebody: Sprite[] = []
 snakebody = []
 atefoodflag = 0
-let snakehead = sprites.create(img`
+snakehead = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -70,7 +72,7 @@ let snakehead = sprites.create(img`
     . . . . . . 3 3 3 3 3 3 3 3 3 3 
     . . . . . . 3 3 3 3 3 3 3 3 3 3 
     `, SpriteKind.Player)
-let snakefood = sprites.create(img`
+snakefood = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -125,8 +127,11 @@ game.onUpdate(function () {
 })
 game.onUpdate(function () {
     for (let value of snakebody) {
-        if (!(value == snakebody[0]) && snakehead.overlapsWith(value)) {
+        if (!(value == snakebody[1]) && !(value == snakebody[0]) && value == snakebody[1] && snakehead.overlapsWith(value)) {
             game.over(false)
         }
+    }
+    if (snakeheadx < 0 || snakeheadx > 160 || (snakeheady < 0 || snakeheady > 120)) {
+    	
     }
 })
